17afe95b4f4b87099fd71137a1347ca3edb6cb83
Vitalie Țurcanu Alexandru <vitalie.turcanu@gmail.com>
2014-10-16 17:01:47 +0300

    meta charset="UTF-8"

--- httpsdocs/index.php
+++ httpsdocs/index.php
@@ -26,7 +26,8 @@ $projectTitle = Config::get('project_name_' . Config::get('user_language'), $cor
 <!DOCTYPE html>
 <html>
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta charset="UTF-8">
     <meta name="author" content="KETSE">
     <meta name="description" content="Casebox">
     <meta name="robots" content="noindex">
