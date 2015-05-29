56f076474e0f1400e36b7c78e2b1597cd2fa4c92
Ghindows <bulgac@gmail.com>
2015-05-29 15:52:50 +0300

    Task • #6216 : SOLR symlinks should use  instead of hardcoded cb_

--- tests/test/InstallTest.php
+++ tests/test/InstallTest.php
@@ -33,7 +33,28 @@ class InstallTest extends \PHPUnit_Framework_TestCase
 
         $this->assertTrue( file_exists($result['links']['log']), 'solr logs configset symlink not created : ' . $result['links']['log'] );
         $this->assertTrue( file_exists($result['links']['default']), 'solr default configset symlink not created : ' .$result['links']['default'] );
+        
 
+    }
+
+    public function testReindexSolr()
+    {
+        /*
+        $argv[1] = '-c';
+        $argv[2] = 'test';
+        $argv[3] = '-a';
+        $argv[4] = '-l'; */
+
+        $content = \CB\UNITTESTS\HELPERS\getIncludeContents(\CB\BIN_DIR . 'solr_reindex_core.php');
+
+        $this->assertEquals('no core specified or invalid options set.', $content);
+
+        $options =  [ 'options' => [ 'core' => DEFAULT_TEST_CORENAME, 'all' => true, 'nolimit' => true ] ];
+
+        $content = \CB\UNITTESTS\HELPERS\getIncludeContents(\CB\BIN_DIR . 'solr_reindex_core.php', $options);
+        $content = array_filter(explode("\n",$content));
+
+        $this->assertEquals('optimizing', end($content) );
     }
     
 }
--- tests/test/SearchTest.php
+++ tests/test/SearchTest.php
@@ -33,17 +33,5 @@ class SearchTest extends \PHPUnit_Framework_TestCase
 
     }
 
-    public function testReindexSolr()
-    {
-        /*
-        $argv[1] = '-c';
-        $argv[2] = 'test';
-        $argv[3] = '-a';
-        $argv[4] = '-l'; */
 
-        $content = \CB\UNITTESTS\HELPERS\getIncludeContents(\CB\BIN_DIR . 'solr_reindex_core.php');
-
-        $this->assertEquals('no core specified or invalid options set.', $content);
-
-    }
 }
