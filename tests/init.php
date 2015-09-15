8004bae5113e55d46eac2e4c8270d7a813270e6e
Ghindows <bulgac@gmail.com>
2015-09-16 01:40:44 +0300

    separate things with testing and autoinstall CASEBOX

--- tests/init.php
+++ tests/init.php
@@ -7,31 +7,12 @@
  * @author ghindows
  */
 
-//define constants
-define('CB_ROOT_PATH', dirname(__DIR__) . DIRECTORY_SEPARATOR);
-define('CB_DOC_ROOT', CB_ROOT_PATH . 'httpsdocs' . DIRECTORY_SEPARATOR);
-
-define('TEST_PATH', __DIR__ . DIRECTORY_SEPARATOR);
-
-define('TEST_PATH_TEMP', TEST_PATH . 'tmp' . DIRECTORY_SEPARATOR);
-
-define('DEFAULT_TEST_CBPREFIX', 'cbtest');
-define('DEFAULT_TEST_CORENAME', 'test');
-define('DEFAULT_TEST_USERNAME', 'root');
-define('DEFAULT_TEST_USERPASS', 'devel');
-
-//create tmp if doesnt exist
-if (!file_exists(TEST_PATH_TEMP)) {
-    mkdir(TEST_PATH_TEMP, 0755, true);
-}
-
+include __DIR__.'/config.php';
 
 include TEST_PATH . '../vendor/autoload.php';
 
 include CB_DOC_ROOT . 'classes/UnitTest/Helpers.php';
 
-UnitTest\Helpers::prepareInstance();
-echo "instance for testing prepared, try to init CASEBOX and start testing".PHP_EOL;
 UnitTest\Helpers::init();
 
 ini_set('display_errors', 1);
