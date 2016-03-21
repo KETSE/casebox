2920e35fe250ef87fe1e3ea625cfe374c440aeff
Ghindows <bulgac@gmail.com>
2016-03-21 18:44:24 +0200

    add  scripts for travis-ci tests integrations (fix path)

--- src/Tests/run.sh
+++ src/Tests/run.sh
@@ -2,10 +2,10 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # first test RPC
-$DIR/../../vendor/bin/phpunit --colors --verbose --debug --configuration $DIR/../../vendor/caseboxdev/rpc-bundle/Tests/phpunit.xml
+$DIR/../../vendor/bin/phpunit --colors --verbose --debug --configuration $DIR/../../vendor/caseboxdev/rpc-bundle/src/Tests/phpunit.xml
 
 # second test REST
-$DIR/../../vendor/bin/phpunit --colors --verbose --debug --configuration $DIR/../../vendor/caseboxdev/rest-bundle/Tests/phpunit.xml
+$DIR/../../vendor/bin/phpunit --colors --verbose --debug --configuration $DIR/../../vendor/caseboxdev/rest-bundle/src/Tests/phpunit.xml
 
 # third test CORE
-$DIR/../../vendor/bin/phpunit --colors --verbose --debug --configuration $DIR/../../vendor/caseboxdev/core-bundle/Tests/phpunit.xml
+$DIR/../../vendor/bin/phpunit --colors --verbose --debug --configuration $DIR/../../vendor/caseboxdev/core-bundle/src/Tests/phpunit.xml
