d1e478b28cf589eadc4290f796a94cd77d5e56a3
Vladimir Melnic <win32hack@gmail.com>
2016-05-10 17:28:23 +0300

    Travis. Updated script.

--- tests/run.sh
+++ tests/run.sh
@@ -5,9 +5,9 @@ set -e
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 # rpc-bundle
-rm -r $DIR/../var/cache/test
+[ -d var/cache/test/ ] && rm -r var/cache/test
 bash $DIR/../vendor/caseboxdev/rpc-bundle/src/Tests/run.sh
 
 # core-bundle
-rm -r $DIR/../var/cache/test
+[ -d var/cache/test/ ] && rm -r var/cache/test
 bash $DIR/../vendor/caseboxdev/core-bundle/src/Tests/run.sh
