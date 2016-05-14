f3b18164974b1e1513d882330088178c14fe35fa
Vladimir Melnic <win32hack@gmail.com>
2016-05-14 18:00:17 +0300

    Fix provision.

--- tests/tests.sh
+++ tests/tests.sh
@@ -4,10 +4,10 @@ set -e
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
-# core-bundle
+echo -e "\n[*] Run core-bundle tests.\n"
 [ -d var/cache/test/ ] && sudo rm -r var/cache/test
 bash $DIR/../vendor/caseboxdev/core-bundle/src/Tests/run.sh
 
-# rpc-bundle
+echo -e "\n[*] Run rpc-bundle tests.\n"
 [ -d var/cache/test/ ] && sudo rm -r var/cache/test
 bash $DIR/../vendor/caseboxdev/rpc-bundle/src/Tests/run.sh
