7dab15bc3cda2686de41adca38a84e0c39fdb498
Vladimir Melnic <win32hack@gmail.com>
2016-05-14 21:22:40 +0300

    Fix provision.

--- tests/phpcs.sh
+++ tests/phpcs.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
-set -e
+
 PHP=$( which php )
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-$PHP $DIR/../vendor/bin/phpcs --config-set installed_paths vendor/escapestudios/symfony2-coding-standard
-$PHP $DIR/../vendor/bin/phpcs --standard=Symfony2 --report-file=$DIR/../build/logs/phpcs.xml --report=xml $DIR/../vendor/caseboxdev/rpc-bundle/src/Service/ $DIR/../vendor/caseboxdev/rpc-bundle/src/Controller/ $DIR/../vendor/caseboxdev/core-bundle/src/Service/ $DIR/../vendor/caseboxdev/core-bundle/src/Controller/
-$PHP $DIR/../vendor/bin/phpcs --standard=Symfony2 --report=checkstyle --report-file=$DIR/../build/logs/checkstyle.xml $DIR/../vendor/caseboxdev/rpc-bundle/src/Service/ $DIR/../vendor/caseboxdev/rpc-bundle/src/Controller/ $DIR/../vendor/caseboxdev/core-bundle/src/Service/ $DIR/../vendor/caseboxdev/core-bundle/src/Controller/
+
+$PHP vendor/bin/phpcs --config-set installed_paths vendor/escapestudios/symfony2-coding-standard
+$PHP vendor/bin/phpcs --standard=Symfony2 --report-file=$DIR/../build/logs/phpcs.xml --report=xml vendor/caseboxdev/rpc-bundle/src/Service/ vendor/caseboxdev/rpc-bundle/src/Controller/ vendor/caseboxdev/core-bundle/src/Service/ vendor/caseboxdev/core-bundle/src/Controller/
+$PHP vendor/bin/phpcs --standard=Symfony2 --report=checkstyle --report-file=$DIR/../build/logs/checkstyle.xml vendor/caseboxdev/rpc-bundle/src/Service/ vendor/caseboxdev/rpc-bundle/src/Controller/ vendor/caseboxdev/core-bundle/src/Service/ vendor/caseboxdev/core-bundle/src/Controller/
--- tests/tests.sh
+++ tests/tests.sh
@@ -1,13 +1,18 @@
 #!/bin/bash bash
+
 set -e
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PHP=$(which php)
+
 echo -e "\n[*] Run core-bundle tests.\n"
 [ -d var/cache/test/ ] && sudo rm -r var/cache/test
 bash $DIR/../vendor/caseboxdev/core-bundle/src/Tests/run.sh
+
 echo -e "\n[*] Run rpc-bundle tests.\n"
 [ -d var/cache/test/ ] && sudo rm -r var/cache/test
 bash $DIR/../vendor/caseboxdev/rpc-bundle/src/Tests/run.sh
+
 echo -e "\n[*] Merge coverage-clover reports to clover.xml file.\n"
 [ -d build/logs/clover.xml ] && sudo rm build/logs/clover.xml
 $PHP $DIR/../bin/clover_merge -o $DIR/../build/logs/clover.xml -f $DIR/../build/logs/clover-core-app.xml -f $DIR/../build/logs/clover-rpc-api.xml -f $DIR/../build/logs/clover-rpc-app.xml
