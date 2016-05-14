6a2b91649e5e0cdccea587f1f6636b7351664d58
Vladimir Melnic <win32hack@gmail.com>
2016-05-14 15:57:18 +0300

    Added jenkins ci provisions.

---
+++ ci/Readme.md
@@ -0,0 +1,17 @@
+Casebox CI
+==========
+
+Jenkins
+-------
+
+URL: [http://ci.casebox.org:8080/job/casebox/job/development/](http://ci.casebox.org:8080/job/casebox/job/development/)
+
+On a clean machine, run once `jenkins/preinstall.sh` script.
+
+After thar run `jenkins/run.sh` command.
+
+
+Travis
+------
+
+Not used any more.
---
+++ ci/jenkins/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT=$(realpath "$DIR/../..")
+echo -e "\n[*] Run provision...\n"
+bash ansible-playbook -i "localhost," -c local $DIR/provision.yml --extra-vars="casebox_core='test' casebox_server_name='development.ci.casebox.org' casebox_root_dir='/var/lib/jenkins/workspace/casebox/development/'"
+echo -e "\n[*] Run tests and sniffers...\n"
+bash $DIR/tests/run.sh
