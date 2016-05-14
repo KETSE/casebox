4cd11c671c8a08a7fbd1c67098753899a5ce1507
Vladimir Melnic <win32hack@gmail.com>
2016-05-14 16:09:32 +0300

    Fix. jenkins ci provisions.

--- ci/jenkins/run.sh
+++ ci/jenkins/run.sh
@@ -2,6 +2,6 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT=$(realpath "$DIR/../..")
 echo -e "\n[*] Run provision...\n"
-bash /usr/bin/ansible-playbook -i "localhost," -c local $DIR/provision.yml --extra-vars="casebox_core='test' casebox_server_name='development.ci.casebox.org' casebox_root_dir='/var/lib/jenkins/workspace/casebox/development/'"
+/usr/bin/ansible-playbook -i "localhost," -c local $DIR/provision.yml --extra-vars="casebox_core='test' casebox_server_name='development.ci.casebox.org' casebox_root_dir='/var/lib/jenkins/workspace/casebox/development/'"
 echo -e "\n[*] Run tests and sniffers...\n"
 bash $ROOT/tests/run.sh
